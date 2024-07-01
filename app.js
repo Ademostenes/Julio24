8d122b2879a9841a9e88c70fbe6856169e7198ca
Demóstenes Acoglanis <126527444+Ademostenes@users.noreply.github.com>
2024-07-01 15:04:08 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,40 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
 app.use('/user',userRoutes);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './index.html');
+    res.sendFile(__dirname + '/public/index.html');
     })
-    
+
+app.get('/inicio', (req, res) => {
+        res.sendFile(__dirname + '/public/index.html');
+        })
+
+app.get('/vuelos', (req, res) => {
+        res.sendFile(__dirname + '/public/vuelos.html');
+        })
+
+app.get('/ingreso', (req, res) => {
+        res.sendFile(__dirname + '/public/ingreso.html');
+        })
+
+app.get('/registro', (req, res) => {
+        res.sendFile(__dirname + '/public/registro.html');
+        })
+
+app.get('/paquetes', (req, res) => {
+       res.sendFile(__dirname + '/public/404.html');
+       })
+
+app.get('/paquetes.html', (req, res) => {
+       res.sendFile(__dirname + '/public/404.html');
+       })
+
+
+
+
 
 const port = 3000;
 
